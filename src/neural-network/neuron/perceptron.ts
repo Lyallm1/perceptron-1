f406fc975a04e64c7f707ac3ded2f3c5ba1fdafb
jbrems <jonasbrems@gmail.com>
2023-01-09 14:08:40 +0100

    Add training data shuffle and function documentation

--- src/neural-network/neuron/perceptron.ts
+++ src/neural-network/neuron/perceptron.ts
@@ -1,15 +1,31 @@
 import { sign } from '../activation';
 import { TrainingData } from '../training-data';
 
+/** Returns a random value between -1 an 1 */
+const randomValue: () => number = () => Math.random() * 2 - 1;
+
+/** Returns a shuffled copy of an array */
+const shuffle: (array: any[]) => any[] = (array: any[]) => {
+  const newArray = [...array];
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+
+  return newArray;
+};
+
 export class Perceptron {
   private weights: number[];
   private biasWeight: number;
 
   constructor (numberOfInputs: number, public learningRate = 0.01) {
-    this.weights = new Array(numberOfInputs).fill(0).map(_ => Math.random() * 2 - 1);
-    this.biasWeight = Math.random() * 2 - 1;
+    this.weights = new Array(numberOfInputs).fill(0).map(randomValue);
+    this.biasWeight = randomValue();
   }
 
+  /** Guesses the output for the given inputs */
   public guess (inputs: number[]): number {
     let sum = inputs.reduce((sum, input, index) => sum + input * this.weights[index], 0);
     sum += this.biasWeight;
@@ -19,19 +35,25 @@ export class Perceptron {
     return guess;
   }
 
+  /** Returns the linear function this perceptron represents (y = mx + b) */
   public get f (): (x: number) => number {
     return (x: number): number => this.m * x + this.b;
   }
 
+  /** Returns the m part of the linear function this perceptron represents (y = mx + b) */
   public get m (): number {
     return -this.weights[0] / this.weights[1];
   }
 
+  /** Returns the b part of the linear function this perceptron represents (y = mx + b) */
   public get b (): number {
     return -this.biasWeight / this.weights[1];
   }
 
-  public train (data: TrainingData[]): void {
+  /** Trains the perceptron with the given training data once */
+  public train (trainingData: TrainingData[]): void {
+    const data: TrainingData[] = shuffle(trainingData);
+
     data.forEach(datum => {
       const guess = this.guess(datum.values);
       const error = datum.label - guess;
